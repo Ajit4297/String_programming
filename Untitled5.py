6a7d9bf4b80d08a0060f9659820e81aa19851346
Ajit4297 <168507427+Ajit4297@users.noreply.github.com>
2024-04-30 09:45:06 +0530

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[85]:
+
+
+s1 = 'welcome'
+s2 = ''
+
+for i in range (len(s1)):
+    if i%2==0 :
+        s2=s2+s1[i]
+    
+    #elif i%2!=0 :
+        s2=s2+s1[i]
+print(s2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
